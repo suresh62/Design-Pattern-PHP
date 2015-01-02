c1c7b62472dcad8944e351b2638732dd9c608512
sureshg <sureshg@como-dev29.(none)>
2015-01-02 09:23:40 +0530

    Strategy Pattern

---
+++ Strategy /strategy-1.php
@@ -0,0 +1,107 @@
+<!-- Based on strategy the function is executed.
+we will have strategy or classes that implements same interface and each will have its own implmentation of the comman function.
+We can change the class we want to execute by passing a key or class
+
+Fallowing example by passing key. -->
+
+<? php
+
+//Strategies implementation
+interface IStartegy{
+	public function showTitle($bookId)
+}
+
+class StrategyCaps implements StrategyInterface {
+    public function showTitle($book_in) {
+        $title = $book_in->getTitle();
+        $this->titleCount++;
+        return strtoupper ($title);
+    }
+}
+
+class StrategyExclaim implements StrategyInterface {
+    public function showTitle($book_in) {
+        $title = $book_in->getTitle();
+        $this->titleCount++;
+        return Str_replace(' ','!',$title);
+    }
+}
+
+class StrategyStars implements StrategyInterface {
+    public function showTitle($book_in) {
+        $title = $book_in->getTitle();
+        $this->titleCount++;
+        return Str_replace(' ','*',$title);
+    }
+}
+
+
+class StrategyContext {
+    private $strategy = NULL; 
+    //bookList is not instantiated at construct time
+    public function __construct($strategy_ind_id) {
+        switch ($strategy_ind_id) {
+            case "C": 
+                $this->strategy = new StrategyCaps();
+            break;
+            case "E": 
+                $this->strategy = new StrategyExclaim();
+            break;
+            case "S": 
+                $this->strategy = new StrategyStars();
+            break;
+        }
+    }
+    public function showBookTitle($book) {
+      return $this->strategy->showTitle($book);
+    }
+}
+
+class Book {
+    private $author;
+    private $title;
+    function __construct($title_in, $author_in) {
+        $this->author = $author_in;
+        $this->title  = $title_in;
+    }
+    function getAuthor() {
+        return $this->author;
+    }
+    function getTitle() {
+        return $this->title;
+    }
+    function getAuthorAndTitle() {
+        return $this->getTitle() . ' by ' . $this->getAuthor();
+    }
+}
+
+
+  writeln('BEGIN TESTING STRATEGY PATTERN');
+  writeln('');
+
+  $book = new Book('PHP for Cats','Larry Truett');
+ 
+  $strategyContextC = new StrategyContext('C');
+  $strategyContextE = new StrategyContext('E');
+  $strategyContextS = new StrategyContext('S');
+ 
+  writeln('test 1 - show name context C');
+  writeln($strategyContextC->showBookTitle($book));
+  writeln('');
+
+  writeln('test 2 - show name context E');
+  writeln($strategyContextE->showBookTitle($book));
+  writeln('');
+ 
+  writeln('test 3 - show name context S');
+  writeln($strategyContextS->showBookTitle($book));
+  writeln('');
+
+  writeln('END TESTING STRATEGY PATTERN');
+
+  function writeln($line_in) {
+    echo $line_in."<br/>";
+  }
+
+
+
---
+++ Strategy /strategy-2.php
@@ -0,0 +1,47 @@
+<? php
+
+
+interface IStrategy{
+	public function doOperation($num1,$num2)
+}
+
+
+Class Addition implements IStrategy 
+{
+	public doOperation($num1,$num2)
+	{
+		return $num1+$num2;
+	}
+}
+
+Class Subtraction implements IStrategy
+{
+	public doOperation($num1,$num2)
+	{
+		return $num1-$num2;
+	}
+}
+
+//This class does not know how the operation is performed it only cares of executing the strategy by
+//getting the appropriate inputs
+Class StrategyContext 
+{
+	private $strategy;
+
+	public funtion __construct($operation)
+	{
+		$this->strategy=$operation;
+	}
+
+	public function executeOperation($num1,$num2)
+	{
+		$this->strategy->doOperation($num1,$num2);
+	}
+}
+
+
+$strategyContext=new StrategyContext(new Addition());
+$strategyContext->executeOperation(5,4); //Executing the operations.
+
+$strategyContext=new StrategyContext(new Subtraction());
+$strategyContext->executeOperation(5,4); 
